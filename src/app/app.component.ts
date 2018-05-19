2ddb938f2034cd3af54de237201d907398476ca7
Aditya Singh <adityasingh222247@gmail.com>
2018-05-19 19:53:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor (private http: HttpClient){}
   title = 'app';
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+testFunction() {
+  console.log("button clicked");
+}
+
+ngOnInit(): void {
+  this.http.get<UserResponse>('https://api.github.com/users/seeschweiler').subscribe(
+    data => {
+      console.log("User Login: " + data.login);
+      console.log("Bio: " + data.bio);
+      console.log("Company: " + data.company);
+    },
+    err => {
+      console.log("Error occured.")
+    }
+  );
+}
+
+
+}
+
+interface UserResponse {
+  login: string;
+  bio: string;
+  company: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule, MatCheckboxModule, MatMenuModule,MatCardModule,MatInputModule,MatSelectModule} from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: AppComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatMenuModule,
+    RouterModule.forRoot(appRoutes),
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
